08ba269290a19d6ec8cd0ada69b1b25ad31ff594
LuisDa <salazarislas.daniel@gmail.com>
2025-11-01 23:06:55 -0600

    Skybox y antorchas colocadas

--- main.cpp
+++ main.cpp
@@ -653,15 +653,15 @@ int main()
 		//Piso 
 
 		model = glm::mat4(1.0);
-		model = glm::translate(model, glm::vec3(0.0f, -100.0f, 0.0f));
-		model = glm::scale(model, glm::vec3(2.0f, 1.0f, 1.5f));
+		model = glm::translate(model, glm::vec3(0.0f, -1.5f, 0.0f));
+		model = glm::scale(model, glm::vec3(2.0f, 1.0f, 1.5f)*1.5f);
 		glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));
 		piso.RenderModel();
 
 		for (const auto& coor : coords) 
 		{
 			model = glm::mat4(1.0f);
-			model = glm::translate(model, glm::vec3(coor[0]*2,coor[1],coor[2]*1.5));
+			model = glm::translate(model, glm::vec3(coor[0]*2,coor[1],coor[2]*1.5)*1.5f);
 			model = glm::scale(model, glm::vec3(5.0f, 5.0f, 5.0f));
 			glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));
 			torchModel.RenderModel();
