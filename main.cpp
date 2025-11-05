f0bbd6486506857325ff876dad17f3cd537489be
devos10 <111657011+devos10@users.noreply.github.com>
2025-11-05 13:43:06 -0600

    Agregando variable para juzgado

--- main.cpp
+++ main.cpp
@@ -772,6 +772,7 @@ int main()
 	//se crean mas luces puntuales y spotlight 
 	glm::mat4 model(1.0);
 	glm::mat4 modelaux(1.0);
+	glm::mat4 modeljuz(1.0);
 	glm::vec3 color = glm::vec3(1.0f, 1.0f, 1.0f);
 	GLuint uniformProjection = 0, uniformModel = 0, uniformView = 0, uniformEyePosition = 0,
 		uniformSpecularIntensity = 0, uniformShininess = 0, uniformTextureOffset = 0;
